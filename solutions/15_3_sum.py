8cfc159588ef3f1023d71ef19577301d7ed90585
shao fei <shao_f@worksap.co.jp>
2018-05-21 17:22:49 +0800

    Accepted

--- solutions/15_3_sum.py
+++ solutions/15_3_sum.py
@@ -5,78 +5,61 @@ class Solution:
         :rtype: List[List[int]]
         """
 
-        def get_repeated_nums(nums):
+        def get_repeated_set(num_list):
             repeated_set = set()
-            prev = nums[0]
-            for curr in nums[1:]:
+            prev = num_list[0]
+            for curr in num_list[1:]:
                 if curr == prev:
                     repeated_set.add(curr)
                 prev = curr
             return repeated_set
 
-        def add_sums_of_unique_nums(unique_nums, sums):
-            for i in range(len(unique_nums) - 1):
-                for j in range(i + 1, len(unique_nums)):
-                    n_1, n_2 = unique_nums[i], unique_nums[j]
-                    added = n_1 + n_2
-                    if sums.get(added) is None:
-                        sums[added] = [(n_1, n_2)]
-                    else:
-                        sums[added].append((n_1, n_2))
+        def is_unique_triplet(triplet, triplets_dict):
+            n_1, n_2, n_3 = triplet[0], triplet[1], triplet[2]
+            return triplets_dict.get(n_1, {}).get(n_2, {}).get(n_3) is None \
+                and triplets_dict.get(n_1, {}).get(n_3, {}).get(n_2) is None \
+                and triplets_dict.get(n_2, {}).get(n_1, {}).get(n_3) is None \
+                and triplets_dict.get(n_2, {}).get(n_3, {}).get(n_1) is None \
+                and triplets_dict.get(n_3, {}).get(n_1, {}).get(n_2) is None \
+                and triplets_dict.get(n_3, {}).get(n_2, {}).get(n_1) is None
 
-        def add_sums_of_repeated_nums(repeated_nums, sums):
-            for n in repeated_nums:
-                added = n * 2
-                if sums.get(added) is None:
-                    sums[added] = [(n, n)]
-                else:
-                    sums[added].append((n, n))
+        def add_to_triplets_dict(triplet, triplets_dict):
+            n_1, n_2, n_3 = triplet[0], triplet[1], triplet[2]
+            if triplets_dict.get(n_1) is None:
+                triplets_dict[n_1] = {n_2: {n_3: True}}
+            elif triplets_dict[n_1].get(n_2) is None:
+                triplets_dict[n_1][n_2] = {n_3: True}
+            else:
+                triplets_dict[n_1][n_2][n_3] = True
 
-        def search(unique_nums, counter_nums, triplets):
+        def search(unique_nums, counter_nums, triplets, counter_num_sign, triplets_dict):
             if len(counter_nums) < 2:
                 # Less then 2 counter nums, impossible to form 3 numbers
                 return
-            unique_counter_nums = list(set(counter_nums))
-            repeated_counter_nums = list(get_repeated_nums(counter_nums))
-            sums = {}
-            add_sums_of_unique_nums(unique_counter_nums, sums)
-            add_sums_of_repeated_nums(repeated_counter_nums, sums)
-            for unique_num in unique_nums:
-                pairs = sums.get(-unique_num)
-                if pairs is not None:
-                    triplets.extend([(unique_num, pair[0], pair[1]) for pair in pairs])
-
-        def get_unique_triplets(triplets):
-            triplets_dict = {}
-            unique_triples = list()
-            for triplet in triplets:
-                n_1, n_2, n_3 = triplet[0], triplet[1], triplet[2]
-                if triplets_dict.get(n_1, {}).get(n_2, {}).get(n_3) is None \
-                        and triplets_dict.get(n_1, {}).get(n_3, {}).get(n_2) is None \
-                        and triplets_dict.get(n_2, {}).get(n_1, {}).get(n_3) is None \
-                        and triplets_dict.get(n_2, {}).get(n_3, {}).get(n_1) is None \
-                        and triplets_dict.get(n_3, {}).get(n_1, {}).get(n_2) is None \
-                        and triplets_dict.get(n_3, {}).get(n_2, {}).get(n_1) is None:
-                    # Unique triplet
-                    unique_triples.append((n_1, n_2, n_3))
-                    if triplets_dict.get(n_1) is None:
-                        triplets_dict[n_1] = {n_2: {n_3: True}}
-                    elif triplets_dict[n_1].get(n_2) is None:
-                        triplets_dict[n_1][n_2] = {n_3: True}
-                    else:
-                        triplets_dict[n_1][n_2][n_3] = True
-            return unique_triples
+            repeated_counter_nums = get_repeated_set(counter_nums)
+            counter_nums_set = set(counter_nums)
+            counter_nums = list(counter_nums_set)
+            for num in unique_nums:
+                for counter_num in counter_nums:
+                    if abs(num) >= abs(counter_num):
+                        other_counter_num = (abs(num) - abs(counter_num)) * counter_num_sign
+                        if (other_counter_num == counter_num and counter_num in repeated_counter_nums) \
+                                or (not other_counter_num == counter_num and other_counter_num in counter_nums_set):
+                            triplet = (num, counter_num, other_counter_num)
+                            if is_unique_triplet(triplet, triplets_dict):
+                                triplets.append(triplet)
+                                add_to_triplets_dict(triplet, triplets_dict)
 
         positives = []
         negatives = []
         zeros = []
-        for num in nums:
-            if num > 0:
-                positives.append(num)
-            elif num < 0:
-                negatives.append(num)
+        for n in nums:
+            if n > 0:
+                positives.append(n)
+            elif n < 0:
+                negatives.append(n)
             else:
-                zeros.append(num)
+                zeros.append(n)
 
         positives = sorted(positives, reverse=True)
         negatives = sorted(negatives)
@@ -84,17 +67,18 @@ class Solution:
             positives.append(0)
             negatives.append(0)
 
-        result = []
+        trs = []  # Triplets
+        td = {}  # Triplet dicts
         # Put 3 zeros first
         if len(zeros) >= 3:
-            result.append((0, 0, 0))
+            trs.append((0, 0, 0))
 
         # 1 positive, 2 negative
-        search(list(set(positives)), negatives, result)
+        search(list(set(positives)), negatives, trs, -1, td)
         # 1 negative, 2 positives
-        search(list(set(negatives)), positives, result)
+        search(list(set(negatives)), positives, trs, 1, td)
 
-        return get_unique_triplets(result)
+        return trs
 
 
 print(Solution().threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]))
