f2caebdcebb263bd0ae75e2e77aae5eeb36a85a2
shao fei <shao_f@worksap.co.jp>
2018-05-22 10:35:57 +0800

    Accepted

--- solutions/16_3_sums_closest.py
+++ solutions/16_3_sums_closest.py
@@ -6,6 +6,15 @@ class Solution:
         :rtype: int
         """
 
+        def is_tried(n_1, n_2, tried_dict):
+            return tried_dict.get(n_1, {}).get(n_2) is not None or tried_dict.get(n_2, {}).get(n_1) is not None
+
+        def add_tried(n_1, n_2, tried_dict):
+            if tried_dict.get(n_1) is None:
+                tried_dict[n_1] = {n_2: True}
+            else:
+                tried_dict[n_1][n_2] = True
+
         def search_closest(ns, value):
             left, right = 0, len(ns) - 1
             middle = (left + right) // 2
@@ -31,11 +40,17 @@ class Solution:
 
         nums = sorted(nums)
         closest = None
+        tried = {}
         for i in range(len(nums) - 2):
             for j in range(i + 1, len(nums) - 1):
-                required = target - nums[i] - nums[j]
-                closest_to_required = search_closest(nums[0:i] + nums[i + 1:j] + nums[j + 1:], required)
-                result = nums[i] + nums[j] + closest_to_required
+                n_i, n_j = nums[i], nums[j]
+                if is_tried(n_i, n_j, tried):
+                    # Already tried this pair
+                    continue
+                add_tried(n_i, n_j, tried)
+                required = target - n_i - n_j
+                closest_to_required = search_closest(nums[j + 1:], required)
+                result = n_i + n_j + closest_to_required
                 if closest is None or abs(target - closest) > abs(target - result):
                     closest = result
 
